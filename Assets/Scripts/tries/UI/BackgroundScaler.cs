4c866a965371bf97afc31e7f0ec9927a2f0aea8c
ChrisAB <cristianbatrin@gmail.com>
2021-01-19 13:29:15 +0200

    Fixed some layering issues

---
+++ Assets/Scripts/tries/UI/BackgroundScaler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float width = ScreenSize.GetScreenToWorldWidth;
+        float height = ScreenSize.GetScreenToWorldHeight;
+        transform.localScale = new Vector3(1*width, 1*height, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/tries/UI/MonsterBGScaler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBGScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float width = ScreenSize.GetScreenToWorldWidth;
+        float height = ScreenSize.GetScreenToWorldHeight;
+        transform.localScale = new Vector3(1f*width, height*0.175f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
